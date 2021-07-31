bdb6ab0f454d56e349a96d2082b7925effbb905d
GitHub Action <action@github.com>
2021-07-31 08:41:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/kinglongchen/apiplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/kinglongchen/apiplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.kinglongchen.apiplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kinglongchen.apiplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/kinglongchen/apiplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/kinglongchen/apiplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.kinglongchen.apiplugin.services
 
+import com.github.kinglongchen.apiplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
